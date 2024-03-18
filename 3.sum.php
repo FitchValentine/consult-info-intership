98ebe33655bdffb88124dc2034bf4932ce54fab3
BornThisWayfitch <90413444+BornThisWayfitch@users.noreply.github.com>
2024-03-18 22:12:03 +0200

    Remake task 3 with adding function

--- 3.sum.php
+++ 3.sum.php
@@ -1,13 +1,20 @@
 <?php
 
-$sum = 0;
+function sumMultiples($limit) {
+    $sum = 0;
 
-for ($i = 1; $i < 1000; $i++) {
-    if ($i % 3 === 0 || $i % 5 === 0) {
-        $sum += $i;
+    for ($i = 1; $i < $limit; $i++) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
     }
+
+    return $sum;
 }
 
-echo "Сумма всех чисел меньше 1000, кратных 3 или 5, равна: " . $sum;
+$limit = 1000;
+$result = sumMultiples($limit);
+
+echo "Сумма всех чисел меньше $limit, кратных 3 или 5, равна: $result";
 
 ?>
